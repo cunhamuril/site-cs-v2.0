bdedce90a1833faceb44e17fc3381cb4e093484d
[cunhamuril] <[murilo.sant@hotmail.com]>
2019-08-08 14:44:37 -0300

    Card reforest and active links

--- src/components/pages/Home.jsx
+++ src/components/pages/Home.jsx
@@ -42,19 +42,37 @@ export default props =>
                     </div>
                 </div>
             </div>
-            <a className="carousel-control-prev" href="#carouselExampleIndicators" role="button" data-slide="prev">
+            <a className="carousel-control-prev" href="#carouselExampleIndicators"
+                role="button" data-slide="prev">
                 <span className="carousel-control-prev-icon" aria-hidden="true"></span>
                 <span className="sr-only">Previous</span>
             </a>
-            <a className="carousel-control-next" href="#carouselExampleIndicators" role="button" data-slide="next">
+            <a className="carousel-control-next" href="#carouselExampleIndicators"
+                role="button" data-slide="next">
                 <span className="carousel-control-next-icon" aria-hidden="true"></span>
                 <span className="sr-only">Next</span>
             </a>
         </div>
 
+        <div className="card border-success my-5 container shadow-sm"
+            id="card-reforest">
+            <div className="card-body text-success">
+                <div id="ico-tree">
+                    <i className="fa fa-tree" /> &nbsp;
+                    <i className="fa fa-tree" /> &nbsp;
+                    <i className="fa fa-tree" />
+                </div>
+                <h5 className="card-title pt-3 text-center" id="title-reforest">
+                    <strong>ESTA EMPRESA REFLORESTA</strong>
+                </h5>
+                <p className="card-text text-dark" id="text-reforest">
+                    Nossos produtos são produzidos somente com madeiras reflorestadas de pinus
+                    e eucalipto com respeito as pessoas e ao meio ambiente.
+                </p>
+            </div>
 
-        {/* Content */}
+        </div>
 
         <Building />
-        
+
     </Main>
--- src/components/template/Header.jsx
+++ src/components/template/Header.jsx
@@ -20,35 +20,59 @@ $(window).on('scroll', function () {
     }
 })
 
-export default props =>
-    <nav className="navbar navbar-expand-lg navbar-dark fixed-top">
-        <a className="navbar-brand" href="/">
-            <img src={logo} alt="Logo" className="logo ml-3" />
-        </a>
-        <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-            <i className="fa fa-bars"></i>
-        </button>
+const Header = () => {
 
-        <div className="collapse navbar-collapse" id="navbarSupportedContent">
-            <ul className="navbar-nav ml-auto">
-                <li className="nav-item">
-                    <a className="nav-link" href="/">Início</a>
-                </li>
-                <li className="nav-item">
-                    <a className="nav-link" href="/sobre">Sobre Nós</a>
-                </li>
-                <li className="nav-item">
-                    <a className="nav-link" href="/produtos">Produtos</a>
-                </li>
-                <li className="nav-item">
-                    <a className="nav-link" href="/servicos">Serviços</a>
-                </li>
-                <li className="nav-item">
-                    <a className="nav-link" href="/localizacao">Localização</a>
-                </li>
-                <li className="nav-item">
-                    <a className="nav-link" href="/contato">Contato</a>
-                </li>
-            </ul>
-        </div>
-    </nav>
+    const isActive = path => {
+        const baseURL = `${window.location.protocol}//${window.location.host}`
+
+        if (window.location.href === `${baseURL}/${path}`) {
+            return 'active'
+        }
+    }
+
+    return (
+        <nav className="navbar navbar-expand-lg navbar-dark fixed-top">
+            <a className="navbar-brand" href="/">
+                <img src={logo} alt="Logo" className="logo ml-3" />
+            </a>
+            <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                <i className="fa fa-bars"></i>
+            </button>
+
+            <div className="collapse navbar-collapse" id="navbarSupportedContent">
+                <ul className="navbar-nav ml-auto">
+                    <li className="nav-item">
+                        <a className="nav-link" id={isActive('')} href="/">Início</a>
+                    </li>
+                    <li className="nav-item">
+                        <a className="nav-link" id={isActive('sobre')} href="/sobre">
+                            Sobre Nós
+                        </a>
+                    </li>
+                    <li className="nav-item">
+                        <a className="nav-link" id={isActive('produtos')} href="/produtos">
+                            Produtos
+                        </a>
+                    </li>
+                    <li className="nav-item">
+                        <a className="nav-link" id={isActive('servicos')} href="/servicos">
+                            Serviços
+                        </a>
+                    </li>
+                    <li className="nav-item">
+                        <a className="nav-link" id={isActive('localizacao')} href="/localizacao">
+                            Localização
+                        </a>
+                    </li>
+                    <li className="nav-item">
+                        <a className="nav-link" id={isActive('contato')} href="/contato">
+                            Contato
+                        </a>
+                    </li>
+                </ul>
+            </div>
+        </nav>
+    )
+}
+
+export default Header;
